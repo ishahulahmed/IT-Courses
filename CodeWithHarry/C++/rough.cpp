651c25e89d8849c504431370c19637fbffb27417
ishahulahmed <shahulahmed1717@gmail.com>
2024-08-05 21:02:44 +0530

    C++

--- CodeWithHarry/C++/rough.cpp
+++ CodeWithHarry/C++/rough.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int c = 44;
 int main()
 {
-      int a, b, c;
-      cout<<"Enter the value of a \n";
-      cin>>a;
-      cout<<"Enter the value of b \n";
-      cin>>b;
-      c = a + b;
-      cout<<"The sum of a and b is "<<c;
+      int c = 22;
+      cout<<c<<endl;
+      cout<<::c<<endl;
       return 0;
 }
